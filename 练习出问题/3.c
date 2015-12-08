e7ae5f374a94af88ce5f97d2004732f2965fdd51
x772334318 <772334318@qq.com>
2015-12-08 17:33:14 +0800

    question

---
+++ 练习出问题/3.c
@@ -0,0 +1,56 @@
+/*
+ * ÈP52“≥  ¡∑œ∞3-2
+ *Author: YuQian Xie <xieceph@163.com>
+ *Date: Nov 8.2015
+ *File: 3.c
+ *
+ *Write a function expand(s1,s2) that expands shorthand notations 
+ *like a-z in the string s1 into the equivalent complete list 
+ *abc...xyz in s2 . Allow for letters of either case and digits, 
+ *and be prepared to handle cases like a-b-c and a-z0-9 and -a-z . 
+ *Arrange that a leading or trailing - is taken literally.
+ */
+#include<stdio.h>
+#define MAXLEN 1024
+
+void expand(char s1[], char s2[]);
+
+main()
+{
+	char s1[MAXLEN], s2[MAXLEN];
+
+	printf("«Î ‰»Î◊÷∑˚¥Æ£∫");
+	scanf("%s",&s1);
+	expand(s1, s2);
+	printf("%s\n", s2);
+
+	return 0;
+}
+
+void expand(char s1[],char s2[])
+{
+	int i, j, n;
+
+	i = j = 0;
+	n = 1;
+	while (s1[i] != '\0') 
+	{
+		if (((s1[i] >= 'a' && s1[i] <= 'z') || (s1[i] >= 'A' && s1[i] <= 'Z') || (s1[i] >= '0' && s1[i] <= '9')) && s1[i+1] == '-')
+		{
+			s2[j] = s1[i];
+			while (s2[j] < s1[i+2])
+			{
+				j++;
+				s2[j] = s1[i] + n;
+				n++;
+			}
+			n = 1;
+		}
+		else
+		{
+			s2[j] = s1[i];
+		}
+		i++;
+		j++;
+	}
+}
